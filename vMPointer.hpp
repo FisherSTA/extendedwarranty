cf46a0830f34b9af6409414fec2dcc7a648cc53e
FisherSTA <fisher.stamour@gmail.com>
2022-01-16 17:59:57 -0800

    add vector moving pointer, a pointer that moves with the vector

---
+++ vMPointer.hpp
@@ -0,0 +1,34 @@
+// copyright 2022 Fisher St. Amour
+// no rights reserved
+
+#include <vector>
+namespace ews
+{
+    template<typename T>
+    class vMPointer
+    {
+    private:
+        std::vector<T>& vector;
+        int index;
+    public:
+        vMPointer(std::vector<T>& vector, T& value)
+            : vector(vector)
+        {
+            index = &value - &vector[0];
+        }
+        vMPointer(int index, std::vector<T>& vector) : index(index), vector(vector){};
+        ~vMPointer(){}
+        int getIndex() {return index;} //returns the index
+        T& getValue() {return vector[index];} //returns a reference to the value pointed to
+        vMPointer<T> operator +(vMPointer<T> other) {return vMPointer<T>(index + other.getIndex(), vector);}
+        vMPointer<T> operator +(int other) {return vMPointer<T>(index + other, vector);}
+        vMPointer<T> operator -(vMPointer<T> other) {return vMPointer<T>(index - other.getIndex(), vector);}
+        vMPointer<T> operator -(int other) {return vMPointer<T>(index - other, vector);}
+        vMPointer<T> operator *(vMPointer<T> other) {return vMPointer<T>(index * other.getIndex(), vector);}
+        vMPointer<T> operator *(int other) {return vMPointer<T>(index * other, vector);}
+        vMPointer<T> operator /(vMPointer<T> other) {return vMPointer<T>(index / other.getIndex(), vector);}
+        vMPointer<T> operator /(int other) {return vMPointer<T>(index / other, vector);}
+        T& operator *() {return vector[index];}
+        T* operator ->() const {return &vector[index];}
+    };
+}
